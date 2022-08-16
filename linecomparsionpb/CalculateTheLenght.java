b4c84d518cd92b5e77b54488857e7040e1c8c035
sammarathe <sanjaymarathe1412@gmail.com>
2022-08-16 23:34:37 +0530

    first commit

---
+++ linecomparsionpb/CalculateTheLenght.java
@@ -0,0 +1,11 @@
+package linecomparsionpb;
+
+public class CalculateTheLenght {
+	public static void main(String[] args) {
+		int x1=1,y1=1,x2=5,y2=5;
+		double distance = Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+		System.out.println(distance);
+		
+	}
+
+}
---
+++ linecomparsionpb/Equals.java
@@ -0,0 +1,14 @@
+package linecomparsionpb;
+
+public class Equals {
+	public static void main(String[] args) {
+	 Long lng1 = 15l;
+	 Long lng2 = 15l;      
+	 Double dbl1 =11.2d;
+	 Double dbl2 =15.d;
+	 
+	 
+	 System.out.println("lng1 and lng2 are equal : "+(lng1.equals(lng2)));  
+     System.out.println("dbl1 and dbl2 are equal : "+(dbl1.equals(dbl2)));
+ }
+}
